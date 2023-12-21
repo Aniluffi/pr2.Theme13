bc8b564ea0f8550c4ffb238400fdf652dea7787e
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2023-12-21 14:04:26 +0200

    Add files via upload

---
+++ task2/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Software sf1 = new Software("ko","b","nok");
+            sf1.ChangeAccess("g");
+            sf1.ChangeAccess("p");
+
+            sf1.ShowInf();
+
+            sf1.J(sf1.Access); 
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task2/ConsoleApp1/Software.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Software
+    {
+        string name;
+        string manufacturer;
+        string access;
+
+        
+
+        public string Name { get => name; set => name = value; }
+        public string Manufacturer { get => manufacturer; set => manufacturer = value; }
+        public string Access 
+        { 
+            get => access; 
+            set
+            {
+                if (value == "p") access = "платное";
+                else if (value == "b") access = "бесплатное";
+                else
+                {
+                   
+                    access = "ошибка";
+                }
+            } 
+        }
+
+        public Software(string manufacturer, string access,string name)
+        {
+            this.Name = name;
+            this.Manufacturer = manufacturer;
+            this.Access = access;
+        }
+
+        public void ChangeAccess(string ch)
+        {
+            Access = ch;
+        }
+
+        public void J(string d)
+        {
+             Console.WriteLine($"Софт {name} является {access}\n");
+
+        }
+
+        public void ShowInf()
+        {
+            Console.WriteLine($"Название - {name}\n" +
+                $"Производитель - {manufacturer}\n" +
+                $"Доступ - {access}\n");
+        }
+    }
+}
